68c2fc79a9903763643749a4c55db2f3321c2c8b
kana-shii <isabella.vasques1806@gmail.com>
2024-10-10 15:53:36 -0300

    explosion effect

---
+++ Assets/Shaders/Nuke/ShaderGraph_Nuke/Scripts/NukeSceneScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukeSceneScript : MonoBehaviour
+{
+    public float nukeDelay;
+    public float cameraShakeDelay;
+    public GameObject missileVFX;
+    public GameObject nukeVFX;
+    public Animation camAnim;
+    public AudioClip nukeSFX;
+    public AudioClip missileSFX;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Nuke());
+            audioSource.PlayOneShot(missileSFX);
+            GameObject vfxMissile = Instantiate(missileVFX) as GameObject;
+            Destroy(vfxMissile, nukeDelay+1);
+        }        
+    }
+
+    IEnumerator Nuke()
+    {
+        yield return new WaitForSeconds(nukeDelay);
+        audioSource.PlayOneShot(nukeSFX);
+        GameObject vfxNuke = Instantiate(nukeVFX) as GameObject;
+        Destroy(vfxNuke, 15);
+
+        yield return new WaitForSeconds(cameraShakeDelay);
+        camAnim.Play();
+    }
+}
